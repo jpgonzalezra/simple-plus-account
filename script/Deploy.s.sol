9107c4a6f09fda276c45e5aee2d49528acc7041e
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-05-21 09:59:09 -0300

    style: fix SPDX-License-Identifier format in solidity files

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,5 +1,5 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity >=0.8.25 <0.9.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.25 <0.9.0;
 
 // import { SimplePlusAccount } from "../src/SimplePlusAccount.sol";
 
