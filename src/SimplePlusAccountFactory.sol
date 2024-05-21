b5d0b10a9f551e92c30be9cc373ab5754da2e7bb
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-05-20 21:01:19 -0300

    style: Improve code formatting and readability

--- src/SimplePlusAccountFactory.sol
+++ src/SimplePlusAccountFactory.sol
@@ -8,7 +8,8 @@ import "./SimplePlusAccount.sol";
 
 /**
  * A sample factory contract for SimplePlusAccount
- * A UserOperations "initCode" holds the address of the factory, and a method call (to createAccount, in this sample factory).
+ * A UserOperations "initCode" holds the address of the factory, and a method call (to createAccount, in this sample
+ * factory).
  * The factory's createAccount returns the target account address even if it is already installed.
  * This way, the entryPoint.getSenderAddress() can be called either before or after the account is created.
  */
@@ -23,30 +24,36 @@ contract SimplePlusAccountFactory {
      * create an account, and return its address.
      * returns the address even if the account is already deployed.
      * Note that during UserOperation execution, this method is called only if the account is not deployed.
-     * This method returns an existing account address so that entryPoint.getSenderAddress() would work even after account creation
+     * This method returns an existing account address so that entryPoint.getSenderAddress() would work even after
+     * account creation
      */
-    function createAccount(address owner,uint256 salt) public returns (SimplePlusAccount ret) {
+    function createAccount(address owner, uint256 salt) public returns (SimplePlusAccount ret) {
         address addr = getAddress(owner, salt);
         uint256 codeSize = addr.code.length;
         if (codeSize > 0) {
             return SimplePlusAccount(payable(addr));
         }
-        ret = SimplePlusAccount(payable(new ERC1967Proxy{salt : bytes32(salt)}(
-                address(accountImplementation),
-                abi.encodeCall(SimplePlusAccount.initialize, (owner))
-            )));
+        ret = SimplePlusAccount(
+            payable(
+                new ERC1967Proxy{ salt: bytes32(salt) }(
+                    address(accountImplementation), abi.encodeCall(SimplePlusAccount.initialize, (owner))
+                )
+            )
+        );
     }
 
     /**
      * calculate the counterfactual address of this account as it would be returned by createAccount()
      */
-    function getAddress(address owner,uint256 salt) public view returns (address) {
-        return Create2.computeAddress(bytes32(salt), keccak256(abi.encodePacked(
-                type(ERC1967Proxy).creationCode,
-                abi.encode(
-                    address(accountImplementation),
-                    abi.encodeCall(SimplePlusAccount.initialize, (owner))
+    function getAddress(address owner, uint256 salt) public view returns (address) {
+        return Create2.computeAddress(
+            bytes32(salt),
+            keccak256(
+                abi.encodePacked(
+                    type(ERC1967Proxy).creationCode,
+                    abi.encode(address(accountImplementation), abi.encodeCall(SimplePlusAccount.initialize, (owner)))
                 )
-            )));
+            )
+        );
     }
 }
